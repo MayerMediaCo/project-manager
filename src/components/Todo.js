2fccf58c0632774c971fee7e21746f977b103e33
Danny Mayer <mayermediaco@gmail.com>
2019-06-05 21:41:48 -0500

    add form logic

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Todo() {
-  return (
-    <div>
-      <h1>Todo's</h1>
-      <form>
-        <input type="text"></input>
-        <button type="submit">Add Task</button>
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    alert('A Task was added: ' + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Task:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type='submit' value='Submit' />
       </form>
-    </div>
-  )
+    );
+  }
 }
 
 export default Todo
