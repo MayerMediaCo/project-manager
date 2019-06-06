57049722fd19e8792ee09c923a65c5933775b96f
Danny Mayer <mayermediaco@gmail.com>
2019-06-05 21:22:58 -0500

    initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+      <h1>Todo's</h1>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  )
+}
+
+export default Todo
