d861dd1078309de0e66633b7a28c07a129994600
Danny Mayer <mayermediaco@gmail.com>
2019-06-06 21:42:36 -0500

    app cleanup

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import CreateTodo from './components/CreateTodo'
-import Todo from './components/Todo'
-import EditTodo from './components/EditTodo'
+import Header from './components/global/Header'
+
+import CreateTodo from './components/routes/CreateTodo'
+import Todo from './components/routes/Todo'
+import EditTodo from './components/routes/EditTodo'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello World</h1>
-        </header>
+      <div className="grid-container">
+        <Header />
+
         <Route path="/" exact component={Todo} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
